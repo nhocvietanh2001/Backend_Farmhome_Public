f530363d40ce244589debf8cd77f38ed011715bf
nhocvietÃanh2001 <nhocvietanh2001@gmail.com>
2022-12-14 11:19:38 +0700

    fix tensorflow

--- src/main/java/com/ute/farmhome/config/Config.java
+++ src/main/java/com/ute/farmhome/config/Config.java
@@ -24,31 +24,31 @@ public class Config {
     {
         return new BCryptPasswordEncoder(BCryptPasswordEncoder.BCryptVersion.$2A,10);
     }
-    @Bean
-    public Graph tfModelGraph(@Value("${fruitdetect.model}") String tfModelPath) throws IOException {
-        Resource graphResource = getResource(tfModelPath);
-        Graph graph = new Graph();
-        graph.importGraphDef(IOUtils.toByteArray(graphResource.getInputStream()));
-        log.info("Loaded Tensorflow model");
-        return graph;
-    }
-
-    private Resource getResource(@Value("${tf.frozenModelPath}") String tfModelPath) {
-        Resource graphResource = new FileSystemResource(tfModelPath);
-        if(!graphResource.exists()) {
-            graphResource = new ClassPathResource(tfModelPath);
-        }
-        if(!graphResource.exists()) {
-            throw new IllegalArgumentException(String.format("File %s does not exist", tfModelPath));
-        }
-        return graphResource;
-    }
-
-    @Bean
-    public List<String> tfModelLabels(@Value("${fruitdetect.labels}") String labelsPath) throws IOException {
-        Resource labelsRes = getResource(labelsPath);
-        log.info("Loaded model labels");
-        return IOUtils.readLines(labelsRes.getInputStream(), Charset.forName("UTF-8")).stream()
-                .map(label -> label.substring(label.contains(":") ? label.indexOf(":")+1 : 0)).collect(Collectors.toList());
-    }
+//    @Bean
+//    public Graph tfModelGraph(@Value("${fruitdetect.model}") String tfModelPath) throws IOException {
+//        Resource graphResource = getResource(tfModelPath);
+//        Graph graph = new Graph();
+//        graph.importGraphDef(IOUtils.toByteArray(graphResource.getInputStream()));
+//        log.info("Loaded Tensorflow model");
+//        return graph;
+//    }
+//
+//    private Resource getResource(@Value("${tf.frozenModelPath}") String tfModelPath) {
+//        Resource graphResource = new FileSystemResource(tfModelPath);
+//        if(!graphResource.exists()) {
+//            graphResource = new ClassPathResource(tfModelPath);
+//        }
+//        if(!graphResource.exists()) {
+//            throw new IllegalArgumentException(String.format("File %s does not exist", tfModelPath));
+//        }
+//        return graphResource;
+//    }
+//
+//    @Bean
+//    public List<String> tfModelLabels(@Value("${fruitdetect.labels}") String labelsPath) throws IOException {
+//        Resource labelsRes = getResource(labelsPath);
+//        log.info("Loaded model labels");
+//        return IOUtils.readLines(labelsRes.getInputStream(), Charset.forName("UTF-8")).stream()
+//                .map(label -> label.substring(label.contains(":") ? label.indexOf(":")+1 : 0)).collect(Collectors.toList());
+//    }
 }
