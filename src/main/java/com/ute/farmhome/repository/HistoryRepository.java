f942679efb028a1a96d3065e49663521cb6fbcb3
nhocvietÃanh2001 <nhocvietanh2001@gmail.com>
2023-01-03 01:54:07 +0700

    refactor get history id descendant

--- src/main/java/com/ute/farmhome/repository/HistoryRepository.java
+++ src/main/java/com/ute/farmhome/repository/HistoryRepository.java
@@ -9,6 +9,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface HistoryRepository extends CrudRepository<History, Integer> {
-    @Query("SELECT h FROM History h WHERE h.farmer.id = :id or h.merchant.id = :id")
+    @Query("SELECT h FROM History h WHERE h.farmer.id = :id or h.merchant.id = :id ORDER BY h.id DESC")
     Page<History> findByFarmerOrMerchantId(int id, Pageable pageable);
 }
